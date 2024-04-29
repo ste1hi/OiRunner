df86f5a73c9703e717ed2158c8013946a76517ef
ste1hi <1874076121@qq.com>
2024-04-29 08:29:34 +0800

    Add readme file

--- OiRunner/BetterRunner.py
+++ OiRunner/BetterRunner.py
@@ -59,7 +59,7 @@ class Functions:
 
     def _output(self, num: int, opt_file: str) -> None:
         '''
-        Merge and output the splited files.
+        Merge and output the split files.
 
         Args:
         num -- The number of files to merge.
@@ -243,7 +243,8 @@ class BetterRunner:
                 if run.returncode != 0:
                     print(f"The return value is{run.returncode}. There may be issues with the program running.")
 
-        except KeyboardInterrupt:
+        # Can't sent Ctrl+c and get the messages.
+        except KeyboardInterrupt:  # pragma: no cover
             if os.path.exists("~tmp"):
                 shutil.rmtree("~tmp")
             print("\nManually exit, wish AC~(^ v ^)")
