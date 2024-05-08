948cfbc48cbf4e437638723faa4416a023083bed
ste1hi <1874076121@qq.com>
2024-05-08 17:04:01 +0800

    Change settings.

--- setup.py
+++ setup.py
@@ -6,8 +6,6 @@ setup(
     author_email = '1874076121@qq.com',
     description = 'This package is designed to help oier compile the cpp file conveniently.',
     url = 'https://github.com/ste1hi/OiRunner',
-    long_description="README.md",
-    long_description_content_type="text/markdown",
     packages = find_packages(),
     python_requires = '>=3.8',
     include_package_data = True,
