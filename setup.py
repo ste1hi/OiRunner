061a4ffb492e67d325c40b72704288c87394087b
ste1hi <1874076121@qq.com>
2024-05-08 16:24:40 +0800

    Change release settings.

--- setup.py
+++ setup.py
@@ -7,8 +7,10 @@ setup(
     author_email = '1874076121@qq.com',
     description = 'This package is designed to help oier compile the cpp file conveniently.',
     url = 'https://github.com/ste1hi/OiRunner',
+    long_description="README.md",
+    long_description_content_type="text/markdown",
     packages = find_packages(),
-    python_requires = '>=3.6',
+    python_requires = '>=3.8',
     include_package_data = True,
     entry_points={
         'console_scripts': [
